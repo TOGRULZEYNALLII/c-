2f493eef1dbf7016a9b598e79e317cf9f74904fd
Togrul Zeynalli <zzzztttt63@gmail.com>
2025-05-16 05:26:36 +0400

    pointers bitdi

---
+++ pointers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+// #include<map>
+#include<set>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    int *p1 = &a; // Pointer to a
+    int *p2 = &b; // Pointer to b
+
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+
+    cout << "Address of a: " << &a << endl;
+    cout << "Address of b: " << &b << endl;
+
+    cout << "Pointer p1 points to value: " << *p1 << endl;
+    cout << "Pointer p2 points to value: " << *p2 << endl;
+
+    return 0;
+}
