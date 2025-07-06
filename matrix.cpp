3c819108214ab922cb52bd80e15f1b0d8f667151
Your Name <zzzztttt63@gmail.com>
2025-07-07 03:48:21 +0400

    test

--- matrix.cpp
+++ matrix.cpp
@@ -906,3 +906,18 @@ using namespace std;
 
 //     return 0;
 // }
+
+///test 
+// int main(){
+//     int n;
+//     cin >> n;
+//     int matrix[n][n];
+//     for(int i = 0; i < n; i++){
+//         for(int j = 0; j < n; j++){
+//             cin >> matrix[i][j];
+
+
+//         }//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
